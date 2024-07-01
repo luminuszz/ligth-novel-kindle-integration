b06231beeca04538ce9e6c28468513debd76dc04
luminuszz <luminuszz43@gmail.com>
2024-07-01 15:05:58 -0300

    fix: fix buffer on send email and add title on pdf

--- src/use-cases/send-email-with.pdfs.ts
+++ src/use-cases/send-email-with.pdfs.ts
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs/promises'
+
 import { resend } from '../lib/resend-mail-provider'
 import { env } from '../utils/env'
 import type { FilesData } from './dto'
@@ -6,13 +8,34 @@ interface SendEmailWithPdfsInput {
   filesPaths: FilesData
 }
 
+interface MailAttachment {
+  content: Buffer
+  filename: string
+}
+
 export async function sendEmailWithPdfs({
   filesPaths,
 }: SendEmailWithPdfsInput) {
-  const pdfFiles = filesPaths.map((file) => ({
-    filename: file.filename,
-    content: Buffer.from(file.filePath),
-  }))
+  const pdfFiles: MailAttachment[] = []
+
+  for (const file of filesPaths) {
+    const currentFile = Bun.file(file.filePath)
+
+    const existsFile = await currentFile.exists()
+
+    if (!existsFile) {
+      throw new Error(`File ${file.filePath} not found`)
+    }
+
+    console.log(
+      `Reading file ${file.filename} with sise ${(currentFile.size / 1024 / 1024).toFixed(4) + 'MB'}`,
+    )
+
+    pdfFiles.push({
+      filename: file.filename,
+      content: Buffer.from(await currentFile.arrayBuffer()),
+    })
+  }
 
   const { error } = await resend.emails.send({
     to: env.KINDLE_EMAIL,
